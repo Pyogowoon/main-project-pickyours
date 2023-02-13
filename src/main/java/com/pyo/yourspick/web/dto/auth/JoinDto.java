002238176255e664fb91241841e2619a6e0174e0
pyo <vytjdgus1234@hanmail.net>
2023-02-13 23:39:00 +0900

    로그인 유효성 검사 강화 구현중

--- src/main/java/com/pyo/yourspick/web/dto/auth/JoinDto.java
+++ src/main/java/com/pyo/yourspick/web/dto/auth/JoinDto.java
@@ -4,6 +4,7 @@ package com.pyo.yourspick.web.dto.auth;
 import com.pyo.yourspick.domain.user.User;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -11,14 +12,16 @@ import javax.validation.constraints.Size;
 @Data
 public class JoinDto {
 
-    @Size(min= 1 , max = 20)
-    @NotBlank
+    @Size(min= 5 , max = 12,message = "크기가 5~12자 사이여야 합니다.")
+    @NotBlank(message = "유저네임을 입력해주세요.")
     private String username;
 
-    @NotBlank
+    //@Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 
-    @NotBlank
+    @Email(message = "올바른 이메일 주소를 입력해주세요.")
+    @NotBlank(message = "이메일 주소를 입력해주세요.")
     private String email;
 
     @NotBlank
